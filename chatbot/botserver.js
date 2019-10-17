0c45f04ca12c6cf6b8a7518491bc7a2309527936
ngrayson <intercomman14@gmail.com>
2019-10-17 16:14:19 -0700

    one error but lots of upgrades!

--- chatbot/botserver.js
+++ chatbot/botserver.js
@@ -1,18 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const path = require('path')
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
 const token = process.env.DBOT_TOKEN;
-console.log('\ntoken: \n '+token)
 
-// client.on('ready', () => {
-// 	console.log('Discord Bot logged in as ' + client.user.tag +'!');
-// });
-
-client.on('error', console.error);
-
-client.login(token).then(result => {
-	console.log(result);
-}, err => {
-	console.log(err)
+client.on('ready', () => {
+	console.log('\x1b[32m%s\x1b[0m%s\x1b[7m%s\x1b[0m',
+		' ✓',' Discord Bot logged in under ', client.user.tag
+		);
 });
+
+client.login(token)
+  .then(console.log('  Discord client logging in...'))
+  .catch(console.error);
--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 // start with npm run dev to run in development mode (refresh on save)
+const TEXTBAR_DOUBLE = '\x1b[36m==================================================\x1b[0m';
+const TEXTBAR_SINGLE = '\x1b[36m--------------------------------------------------\x1b[0m';
+
+console.log('\x1b[36m%s','\n\n');
+console.log(TEXTBAR_DOUBLE);
+console.log(' Starting Loom...\n');
+
 require('dotenv').config()
 
 // express bits
@@ -7,24 +14,31 @@ const webApp = express();
 const PORT = 3000;
 const router = require('./server/webserver');
 
-webApp.listen(PORT, function() {
-	console.log(
-		'\n==============================\n' +
-		'listening to webserver on ' + PORT +
-		'\n==============================\n') ;
+webApp.listen(PORT, () => {
+	console.log('\x1b[32m%s\x1b[0m%s\x1b[7m%s\x1b[0m',
+		' ✓',
+		' Webserver listening on port ', PORT) ;
 })
+console.log('  Webserver app initializing...');
 
 webApp.set('view engine', 'ejs')
 
 webApp.use(router);
 
 
+
+// Discord bits
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = process.env.DBOT_TOKEN;
 
 client.on('ready', () => {
-	console.log('Discord Bot logged in as ' + client.user.tag +'!');
+	console.log('\x1b[32m%s\x1b[0m%s\x1b[7m%s\x1b[0m',
+		' ✓',' Discord Bot logged in under ', client.user.tag
+		);
 });
 
-client.login(token).then(console.log).catch(console.error);
+client.login(token)
+  .then(console.log('  Discord client logging in...'))
+  .catch(console.error);
+console.log(TEXTBAR_SINGLE);

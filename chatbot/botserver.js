483d8cd9dd28a65084a529887f7fea2cc5fae181
Nick Grayson <ngrayson@blizzard.com>
2019-10-18 10:33:11 -0700

    added some flags and changed botserver.js

--- chatbot/botserver.js
+++ chatbot/botserver.js
@@ -1,18 +1,7 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const path = require('path')
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
-const token = process.env.DBOT_TOKEN;
-console.log('\ntoken: \n '+token)
+function message(msg) {
+	console.log('recieved message:\n  ' + msg);
+}
 
-// client.on('ready', () => {
-// 	console.log('Discord Bot logged in as ' + client.user.tag +'!');
-// });
-
-client.on('error', console.error);
-
-client.login(token).then(result => {
-	console.log(result);
-}, err => {
-	console.log(err)
-});
+module.exports = {
+	message
+}

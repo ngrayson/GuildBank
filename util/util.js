12861413b3b1e7ce95f94df6ee0c866b32ea9a19
Nick Grayson <intercomman14@gmail.com>
2020-02-23 20:38:52 -0800

    yet more reload refactoring

--- util/util.js
+++ util/util.js
@@ -1,13 +1,54 @@
-const jsUtil = require('util');
-const promisify = jsUtil.promisify;
-const fs = require('fs');
-fs.readdirAsync = promisify(fs.readdir);
-
 const BAR_WIDTH = 50;
 
 const BAR_PLUS   = '\x1b[36m' + '+'.repeat(BAR_WIDTH) + '\x1b[0m';
 const BAR_SINGLE = '\x1b[36m' + '-'.repeat(BAR_WIDTH) + '\x1b[0m';
 const BAR_DOUBLE = '\x1b[36m' + '='.repeat(BAR_WIDTH) + '\x1b[0m';
+
+/* command reloading code */
+
+const jsUtil = require('util');
+const promisify = jsUtil.promisify;
+const fs = require('fs');
+fs.readdirAsync = promisify(fs.readdir);
+
+const RELATIVE_CMD_PATH = `../chatbot/cmds/`;
+const ABSOLUTE_CMD_PATH = `./chatbot/cmds/`;
+
+async function reloadBotCommands(bot) {
+    if(bot.constructor.name != "Client") throw 'cannot reload bot commands without a bot.'
+    try {
+		bot.commands.forEach( cmd => {
+			delete require.cache[require.resolve(RELATIVE_CMD_PATH + `${cmd.help.name}.js`)];
+		})
+	} catch(e) {
+		log(e,true)
+    }
+	let numCmds;
+	try {
+		let files = await fs.readdirAsync(ABSOLUTE_CMD_PATH)
+		let jsfiles = files.filter(f => f.split(".").pop() === "js");
+		if(jsfiles.length <= 0) {
+			log("No commands to load!", true);
+			return 0;
+		}
+		let numCmds = jsfiles.length
+		log(`    Loading ${numCmds} commands!`,true);
+
+		jsfiles.forEach((f, i) => {
+			let props = require(RELATIVE_CMD_PATH + `${f}`);
+			log(`      command ${i+1}: ${f} loaded!`,true);
+			bot.commands.set(props.help.name, props);
+		});
+		log(`resolving with ${numCmds} commands`,true)
+		return numCmds;
+	}catch(e) {
+		log('Error, a real bad one',true)
+		log(e,true)
+	}
+    return true;
+}
+
+/* end of command reloading code */
 
 function log(str,logToCLI) {
 	// write to file
@@ -99,41 +140,6 @@ function isUser(obj){
 
 function asciiTable(array) {
     // returns a neat string ascii table of the given array
-}
-
-async function reloadBotCommands(bot) {
-    if(bot.constructor.name != "Client") throw 'cannot reload bot commands without a bot.'
-    try {
-		bot.commands.forEach( cmd => {
-			delete require.cache[require.resolve(`../chatbot/cmds/${cmd.help.name}.js`)];
-		})
-	} catch(e) {
-		log(e,true)
-    }
-	// bot.commands = new Discord.Collection();
-	let numCmds;
-	try {
-		let files = await fs.readdirAsync('./chatbot/cmds/')
-		let jsfiles = files.filter(f => f.split(".").pop() === "js");
-		if(jsfiles.length <= 0) {
-			log("No commands to load!", true);
-			return 0;
-		}
-		let numCmds = jsfiles.length
-		log(`    Loading ${numCmds} commands!`,true);
-
-		jsfiles.forEach((f, i) => {
-			let props = require(`../chatbot/cmds/${f}`);
-			log(`      command ${i+1}: ${f} loaded!`,true);
-			bot.commands.set(props.help.name, props);
-		});
-		log(`resolving with ${numCmds} commands`,true)
-		return numCmds;
-	}catch(e) {
-		log('Error, a real bad one',true)
-		log(e,true)
-	}
-    return true;
 }
 
 module.exports = {

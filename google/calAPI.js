59417b577c64cad7e72b33ac12a3ecc7304a32ed
Nick Grayson <intercomman14@gmail.com>
2020-03-03 10:57:44 -0800

    google was the wrong solution

--- google/calAPI.js
+++
@@ -1,9 +0,0 @@
-const Auth = require('./auth.js');
-const {google} = require('googleapis')
-
-async function calendarApi(){
-    let auth = await Auth();
-    return google.calendar({version: 'v3', auth: auth, calendarId: 'primary',});
-}
-
-module.exports = calendarApi;

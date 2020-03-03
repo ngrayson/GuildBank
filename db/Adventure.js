2a848c0af96a8cc5fad99dcc121f04c411acf7c0
Nick Grayson <intercomman14@gmail.com>
2020-03-03 10:58:39 -0800

    more cleanup

--- db/Adventure.js
+++ db/Adventure.js
@@ -5,7 +5,6 @@ const log = util.log;
 const db = require('./db.js');
 const mongoose = db.mongoose;
 const greenCheck = '\x1b[32m ✓ \x1b[0m';
-const calendarApi = require('../google/calAPI.js')
 
 let adventureSchema = new mongoose.Schema({
     options: Boolean
@@ -16,20 +15,7 @@ let adventureSchema = new mongoose.Schema({
 })
 
 adventureSchema.statics.test = async function(adventureObj) {
-    try {
-        let calendar = await calendarApi();
-        calendar.events.list({
-            maxAttendees: 8,
-            maxResults: 10,
-            orderBy: 'date',
-            singleEvents:true
-        }).then( res => {
-            log(res,true)
-        })
-    }
-    catch(e) {
-        log(e,true)
-    }
+
 }
 
 adventureSchema.statics.newAdventure = function(adventureObj) {
